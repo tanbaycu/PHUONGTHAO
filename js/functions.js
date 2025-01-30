9a0bf44c592d5218cb4a38c752901ef1c0b6a7fa
Tran Minh Tan <166855109+tanbaycu@users.noreply.github.com>
2025-01-30 20:27:25 +0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -92,28 +92,34 @@ function startHeartAnimation() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
-	$("#elapseClock").html(result);
+function timeElapse() {
+    var birthDate = new Date(2008, 0, 30); // (Năm, Tháng - 0, Ngày)
+    var current = new Date();
+    var seconds = Math.floor((current - birthDate) / 1000);
+
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds %= (3600 * 24);
+
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) hours = "0" + hours;
+    seconds %= 3600;
+
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) minutes = "0" + minutes;
+    seconds %= 60;
+
+    if (seconds < 10) seconds = "0" + seconds;
+
+    var result = `<span class="digit">${days}</span> ngày 
+                  <span class="digit">${hours}</span> giờ 
+                  <span class="digit">${minutes}</span> phút 
+                  <span class="digit">${seconds}</span> giây`;
+
+    $("#elapseClock").html(result);
 }
 
+// Cập nhật thời gian mỗi giây
+setInterval(timeElapse, 1000);
 function showMessages() {
 	adjustWordsPosition();
 	$('#messages').fadeIn(5000, function() {
